321e9b2df28e342adbb1476c4d2be03b5d157c20
Laisliviaa <lais.lsmarinho@senacsp.edu.br>
2025-06-08 19:12:40 -0300

    Update excluir_livros.php

--- PI FINAL/excluir_livros.php
+++ PI FINAL/excluir_livros.php
@@ -6,12 +6,9 @@ if(!empty($_GET['id'])){
     $sqlSelect = "SELECT * FROM livros WHERE id=$id";
     $resultado = $conexao->query($sqlSelect);
     
-if($resultado->num_rows > 0)
-{
+    if($resultado->num_rows > 0){
     $sqlDelete="DELETE FROM livros WHERE id=$id";
     $resultado = $conexao->query($sqlDelete);
-    header('Location: listar_livros.php');
-
-}}
-
+    header('Location: listar_livros.php');}
+}
 ?>
