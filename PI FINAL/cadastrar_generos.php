2ce0b7d809760e226f22fe5bb58df126d2beea4e
yasmin carolina <yaascarol@hotmail.com>
2025-06-08 18:16:07 -0300

    Update cadastrar_generos.php

--- PI FINAL/cadastrar_generos.php
+++ PI FINAL/cadastrar_generos.php
@@ -21,8 +21,8 @@ if(isset($_POST['submit'])){
 <body>
 
         <nav>
-        <h3>SISTEMA DEWEY</h3>
-        <h1>ADMINISTRADOR - <?php echo htmlspecialchars(strtoupper($nome_administrador)); ?></h1>
+        <h3>ADMINISTRADOR - <?php echo htmlspecialchars(strtoupper($nome_administrador)); ?></h3>
+        <h1>CADASTRAR GÊNEROS</h1>
           <div class="menu">
             <button class="home"><a href="index.php"><img src="imagens/voltar.png"><h6>HOME</a><h6></button>
             <ul>
