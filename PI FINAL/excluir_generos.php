b5ae2ff3763ecaccae397709fd9d159b9a9cccb1
Laisliviaa <lais.lsmarinho@senacsp.edu.br>
2025-06-08 19:10:25 -0300

    Update excluir_generos.php

--- PI FINAL/excluir_generos.php
+++ PI FINAL/excluir_generos.php
@@ -3,21 +3,12 @@ include_once ('conexao.php');
 
 if(!empty($_GET['id'])){
     $id = $_GET['id'];
-    $sqlSelect = "SELECT * FROM generos WHERE id=$id";
+    $sqlSelect = "SELECT * FROM livros WHERE id=$id";
     $resultado = $conexao->query($sqlSelect);
     
     if($resultado->num_rows > 0){
-        $sqlDelete="DELETE FROM generos WHERE id=$id";
-        $resultado = $conexao->query($sqlDelete);
-        header('Location: listar_generos.php');
-        exit();
-    }
-    else{
-        header('Location: listar_generos.php');
-        exit();
-    }
-} else {
-    header('Location: listar_generos.php');
-    exit();
+    $sqlDelete="DELETE FROM livros WHERE id=$id";
+    $resultado = $conexao->query($sqlDelete);
+    header('Location: listar_livros.php');}
 }
 ?>
