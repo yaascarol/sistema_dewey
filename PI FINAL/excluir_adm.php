0cce9d55924c872ed8e2fbcb39bafb4492c218ea
Laisliviaa <lais.lsmarinho@senacsp.edu.br>
2025-06-08 19:09:23 -0300

    Update excluir_adm.php

--- PI FINAL/excluir_adm.php
+++ PI FINAL/excluir_adm.php
@@ -6,15 +6,9 @@ if(!empty($_GET['id'])){
     $sqlSelect = "SELECT * FROM administradores WHERE id=$id";
     $resultado = $conexao->query($sqlSelect);
     
-if($resultado->num_rows > 0){
-{
+    if($resultado->num_rows > 0){
     $sqlDelete="DELETE FROM administradores WHERE id=$id";
     $resultado = $conexao->query($sqlDelete);
-}
-    }
-    else{
-        header('Location: listar_adm.php');
-        
-    }
+    header('Location: listar_adm.php'); }
 }
 ?>
