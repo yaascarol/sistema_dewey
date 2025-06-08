ab0cbe36264a8488e7d678faa463742e449f39b7
yasmin carolina <yaascarol@hotmail.com>
2025-06-08 18:03:12 -0300

    Update editar_adm.php

--- PI FINAL/editar_adm.php
+++ PI FINAL/editar_adm.php
@@ -41,50 +41,47 @@ if($resultado->num_rows > 0){
 </head>
 
 <body>
-    <nav>
-        <h3>SISTEMA DEWEY - (NOME)</h3>
-        <h1>EDITAR ADMINISTRADOR</h1>
-        <div class="menu">
+    
+        <nav>
+        <h3>SISTEMA DEWEY</h3>
+        <h1>ADMINISTRADOR - <?php echo htmlspecialchars(strtoupper($nome_administrador)); ?></h1>
+          <div class="menu">
+            <button class="home"><a href="index.php"><img src="imagens/voltar.png"><h6>HOME</a><h6></button>
             <ul>
-                <li class="menu-icon"><img src="imagens/332-3321096_mobile-menu-brown-menu-icon-png - Copia.png" alt="">
-                    <ul>
-                        <li class="menu-dropdown">Administrador
-                            <ul class="menu-dropdown-right">
-                                <li><a href="cadastrar_adm.php">Cadastrar</a></li>
-                                <li><a href="listar_adm.php">Listar</a></li>
-                            </ul>
-                        </li>
-                        <li class="menu-dropdown">Livros
-                            <ul class="menu-dropdown-right">
-                                <li><a href="cadastrar_livros.php">Cadastrar</a></li>
-                                <li><a href="listar_livros.php">Listar</a></li>
-                            </ul>
-                        </li>
-                        <li class="menu-dropdown">Gêneros
-                            <ul class="menu-dropdown-right">
-                                <li><a href="cadastrar_generos.php">Cadastrar</a></li>
-                                <li><a href="listar_generos.php">Listar</a></li>
-                            </ul>
-                        </li>
-                        <li class="menu-dropdown">Clientes
-                            <ul class="menu-dropdown-right">
-                                <li><a href="cadastrar_clientes.php">Cadastrar</a></li>
-                                <li><a href="listar_clientes.php">Listar</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-            <button class="search"><input type="search" placeholder="Consultar..."> <img
-                    src="imagens/search-icon-png-21.png"></button>
-            <button class="user">
-                <a href="login.php">
-                    <img src="imagens/logout-icon-2048x2048-libuexip.png">
-                    <h6>SAIR</h6>
-                </a>
-            </button>
-        </div>
+              <li class="menu-icon"><img src="imagens/332-3321096_mobile-menu-brown-menu-icon-png.png" alt="">
+                  <ul>
+                      <li class="menu-dropdown">Administrador
+                          <ul class="menu-dropdown-right">
+                              <li><a href="cadastrar-adm.php">Cadastrar</a></li>
+                              <li><a href="listar-adm.php">Listar</a></li>
+                          </ul>
+                      </li>
+                      <li class="menu-dropdown">Livros
+                      <ul class="menu-dropdown-right">
+                          <li><a href="cadastrar-livro.php">Cadastrar</a></li>
+                          <li><a href="listar-livro.php">Listar</a></li>
+                      </ul>
+                      </li>
+                      <li class="menu-dropdown">Gêneros
+                          <ul class="menu-dropdown-right">
+                              <li><a href="cadastrar-genero.php">Cadastrar</a></li>
+                              <li><a href="listar-genero.php">Listar</a></li>
+                          </ul>
+                      </li>
+                      <li class="menu-dropdown">Leitores
+                          <ul class="menu-dropdown-right">
+                              <li><a href="cadastrar-leitor.php">Cadastrar</a></li>
+                              <li><a href="listar-leitor.php">Listar</a></li>
+                          </ul>
+                      </li>
+                  </ul>
+              </li>
+          </ul>
+          <button class="search"><input type="search" placeholder="Consultar..."> <img src="imagens/search-icon-png-21.png"></button>
+          <button class="user"><a href="login.php"><img src="imagens/logout-icon-2048x2048-libuexip.png"><h6>SAIR</a><h6></button>
+    </div>
     </nav>
+
     <div class="container">
         <form class="dados" action="atualizar_adm.php" method="POST">
             <div id="dados-esquerda">
